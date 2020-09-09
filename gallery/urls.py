c5098fc94a87882f42e8d63b4d1b45ef236c4d6d
Vadim G <rrretry@ya.ru>
2020-09-09 13:50:06 +0500

    implement image operations

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from gallery.views import ImageListCreateView
+from gallery.views import *
 
 app_name = 'gallery'
 
 urlpatterns = [
-    path('', ImageListCreateView.as_view(), name='image')
+    path('', ImageListCreateView.as_view(), name='image'),
+    path('<int:pk>', ImageRUDView.as_view(), name='image'),
     # comment
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -8,3 +8,9 @@ class ImageListCreateView(generics.ListCreateAPIView):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
     http_method_names = ['get', 'post']
+
+
+class ImageRUDView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+    http_method_names = ['get', 'put', 'delete']
